323dc172a1214ca06e4cd5dc07f2c4dd074aeea6
Anton Shevchuk <AntonShevchuk94@gmail.com>
2026-09-04 18:46:50 +0300

    Complete Spring HelloWorld task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean==bean2);
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.toString());
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.toString());
+        System.out.println(bean3==bean4);
+
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Cat {
+    private String name;
+    private int age;
+
+    public Cat(){
+
+    }
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return " name='" + name + '\'' +
+                ", age=" + age
+                ;
+    }
 }
